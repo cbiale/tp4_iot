037e2d47ad3e8bf3fa9ce0e5887d17c7feeac306
cbiale <claudio_biale@yahoo.com.ar>
2021-06-16 23:20:06 -0300

    test inicial

---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
+
+describe('Selenium-WebDriver Test', () => {
+    const driver = new Builder().forBrowser('chrome').build();
+
+    it('Buscar la palabra mongoose esp32 y hacer clic en el tercer resultado no patrocinado', async () => {
+        await driver.get('https://www.google.com');
+        await driver.sleep(2000);
+        // palabra a buscar: riot os esp32
+        await driver.findElement(By.name('q')).sendKeys('riot os esp32', Key.ENTER);
+        // almaceno en datos los elemento que contienen URL de los resultados de la búsqueda
+        let datos = await driver.findElements(By.className("yuRUbf"));
+        // si la cantidad de datos es mayor o igual a 3
+        if (datos.length >= 3) {
+            // realizo click en el tercer resultado
+            await datos     [2].click();
+            await driver.sleep(2000);
+            // obtengo el URL que hasta la prueba debe ser https://github.com/gschorcht/RIOT-Xtensa-ESP
+            let url = await driver.getCurrentUrl();
+            // comparo
+            expect(url).to.equal('https://github.com/gschorcht/RIOT-Xtensa-ESP');
+        }
+    });
+
+    after(async () => driver.quit());
+});
+
